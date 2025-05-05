8d2395763eac2fdd9ed1eb491a08b8a916d12b90
Matthias Wirth <matthias.wirth@gmail.com>
2025-05-05 16:45:26 +0000

    reduce filesize by reducing coordinate precision a bit

--- update.sh
+++ update.sh
@@ -2,8 +2,11 @@
 set -e
 
 wget -O tfrs.geojson "http://tfr.faa.gov/geoserver/TFR/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=TFR:V_TFR_LOC&outputFormat=json"
-jq < tfrs.geojson '.crs.properties.name = "urn:ogc:def:crs:OGC:1.3:CRS84"' > tfrs.geojson.tmp
-mv tfrs.geojson.tmp tfrs.geojson
+jq '.crs.properties.name = "urn:ogc:def:crs:OGC:1.3:CRS84"' < tfrs.geojson > tfrs.geojson.tmp
+# reduce number precision
+sed -E -e 's/\s*([0-9-]+\.[0-9]{5})[0-9]*/\1/' < tfrs.geojson.tmp | jq -c > tfrs.geojson
+
+#exit
 
 git add tfrs.geojson
 git commit --amend --date "$(date)" -m "aux update"
